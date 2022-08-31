ecac75a69060965ec35b751a0177fceb38debc6b
Kasun24 <kasun961224@gmail.com>
2022-08-31 21:35:23 +0530

    footer done

--- wp-content/themes/dream-scapers/footer.php
+++ wp-content/themes/dream-scapers/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -10,16 +11,29 @@
  */
 
 ?>
+<div class="footer" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/inner-banner.jpg');">
+    <div class="row">
+        <div class="col-sm-7">
+            <div class="text-left">
+                <img class="footer_logo" src="<?php echo get_template_directory_uri(); ?>/inc/img/logo.png" alt="">
+                <span>Copyright @<script>document.write(new Date().getFullYear())</script> Dreamscaper Publishing</span>
+            </div>
+        </div>
+        <div class="col-sm-5">
+            <div class="text-right">
+                <span>Developed by <a target="_blank" href="https://mozita.digital/">Mozita Digital</a></span>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+<!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/inc/js/script.js"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/inc/js/script.js"></script>
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
@@ -28,4 +42,5 @@
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
