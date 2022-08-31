c96d4283340115f8d5d0f1da00f617df7f526892
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-08-31 17:51:51 +0530

    links done

--- wp-content/themes/dream-scapers/page-home.php
+++ wp-content/themes/dream-scapers/page-home.php
@@ -23,38 +23,38 @@ get_header();  ?>
 </div>
 <!-- icon_section -->
 <div class="icon_section">
-    <a class="icon icon_1 icon_sm anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_2 icon_md anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_2 icon_md social" target="_blank" href=""> <img src="inc/img/icons/fb.png" alt=""></a>
-    <a class="icon icon_3 icon_lg anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_4 icon_md anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_4 icon_md social" target="_blank" href=""> <img src="inc/img/icons/telegram.png" alt=""></a>
-    <a class="icon icon_5 icon_md anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_5 icon_md social" target="_blank" href=""> <img src="inc/img/icons/twitter.png" alt=""></a>
-    <a class="icon icon_6 icon_sm anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_7 icon_sm anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_8 icon_lg anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_8 icon_lg social" target="_blank" href=""> <img src="inc/img/icons/discord.png" alt=""></a>
-    <a class="icon icon_9 icon_sm anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_10 icon_md anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_10 icon_md social" target="_blank" href=""> <img src="inc/img/icons/linkedin.png" alt=""></a>
-    <a class="icon icon_11 icon_md anm" target="_blank" href=""> <img src="inc/img/icons/none.png" alt=""></a>
-    <a class="icon icon_11 icon_md social" target="_blank" href=""> <img src="inc/img/icons/insta.png" alt=""></a>
+    <a class="icon icon_1 icon_sm anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_2 icon_md anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_2 icon_md social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/fb.png" alt=""></a>
+    <a class="icon icon_3 icon_lg anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_4 icon_md anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_4 icon_md social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/telegram.png" alt=""></a>
+    <a class="icon icon_5 icon_md anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_5 icon_md social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/twitter.png" alt=""></a>
+    <a class="icon icon_6 icon_sm anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_7 icon_sm anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_8 icon_lg anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_8 icon_lg social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/discord.png" alt=""></a>
+    <a class="icon icon_9 icon_sm anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_10 icon_md anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_10 icon_md social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/linkedin.png" alt=""></a>
+    <a class="icon icon_11 icon_md anm" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/none.png" alt=""></a>
+    <a class="icon icon_11 icon_md social" target="_blank" href=""> <img src="<?php echo get_template_directory_uri(); ?>/inc/img/icons/insta.png" alt=""></a>
 </div>
 <div class="back_imgs">
-    <img class="left_hand" src="inc/img/left_hand.png" alt="">
-    <img class="heart" src="inc/img/heart.png" alt="">
-    <img class="right_hand" src="inc/img/right_hand.png" alt="">
+    <img class="left_hand" src="<?php echo get_template_directory_uri(); ?>/inc/img/left_hand.png" alt="">
+    <img class="heart" src="<?php echo get_template_directory_uri(); ?>/inc/img/heart.png" alt="">
+    <img class="right_hand" src="<?php echo get_template_directory_uri(); ?>/inc/img/right_hand.png" alt="">
 </div>
 <div class="key_icon">
-    <img class="img-fluid" src="inc/img/key.png" alt="">
+    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/key.png" alt="">
 </div>
 </div>
 <!-- parallax Section 1 -->
-<div class="parallax parallax_1"></div>
+<div class="parallax parallax_1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/par_1.jpg');"></div>
     <!-- Content section 1 -->
     <div class="content_1">
-        <img class="white_sec" src="inc/img/white_section.png" alt="">
+        <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
         <div class="caption header">
             <span class="gold">
                 We invited you to come for Wealth and, if you'd like, stay for the Revolution.
@@ -62,10 +62,10 @@ get_header();  ?>
         </div>
     </div>
     <!-- parallax Section 2 -->
-    <div class="parallax parallax_2"></div>
+    <div class="parallax parallax_2" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/par_2.jpg');"></div>
     <!-- Content section 2 -->
     <div class="content_2">
-        <img class="white_sec" src="inc/img/white_section.png" alt="">
+        <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
         <div class="caption caption_1">
             <span>In a world driven by value-extracion and exploitative principles, we, here at Dreamscapers Publishing,
                 Inc., are building an ecosystem grounded in contribute and collaborative value principles. <br> By
@@ -77,10 +77,10 @@ get_header();  ?>
         </div>
     </div>
     <!-- parallax Section 3 -->
-    <div class="parallax parallax_3"></div>
+    <div class="parallax parallax_3" style="background-image: url('<?php echo get_template_directory_uri(); ?>/inc/img/par_3.jpg');"></div>
     <!-- Content section 3 -->
     <div class="content_3">
-        <img class="white_sec" src="inc/img/white_section.png" alt="">
+        <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
         <div class="caption caption_2">
             <span>At Dreamscaper Publishing. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
                 strive to someday be acquired by a monopoly corporation. <br> We believe in the power of the Interferes
