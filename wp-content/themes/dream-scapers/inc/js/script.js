fd14034ff8b0e12e8ec46481c6a9fb11d7b72cb9
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-09-01 15:51:59 +0530

    test

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -1,5 +1,6 @@
 
 // Preloader
 function closePreloader() {
-    document.getElementById("preloader").style.display = "none";
+    // document.getElementById("preloader").style.display = "none";
+    document.getElementById("preloader").style.animation = "hide_slowly 2s infinite linear";
   }
