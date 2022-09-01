07ea70bbfbecd555662a84c4e17f37981bc47e55
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-09-01 15:59:50 +0530

    test preloader

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -1,7 +1,7 @@
 
 // Preloader
 function closePreloader() {
-    document.getElementById("preloader").style.animation = "hide_slowly 1s";
+    // document.getElementById("preloader").style.animation = "hide_slowly 1s";
     const element = document.getElementById("preloader");
     element.remove();
     // document.getElementById("preloader").style.display = "none";
