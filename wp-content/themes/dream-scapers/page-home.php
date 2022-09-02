45816d7b8e52138aba45a7fc854099cc28ae7901
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-09-02 12:01:45 +0530

    test

--- wp-content/themes/dream-scapers/page-home.php
+++ wp-content/themes/dream-scapers/page-home.php
@@ -62,7 +62,7 @@ get_header();  ?>
 <div class="content_2">
     <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
     <div class="caption caption_1">
-        <span>In a world driven by value-extracion and exploitative principles, we, here at Dreamscapers Publishing Inc,
+        <span>In a world driven by value-extracion and exploitative principles, we, here at Dreamscaper Publishing Inc,
             Inc., are building an ecosystem grounded in contribute and collaborative value principles. <br> By
             contributing and leveraging Our entire intellectual Property portfolio (from technology-related to
             brand-related and artistic-related IP), and sharing our earnings for the benefit of the many rather that
@@ -77,7 +77,7 @@ get_header();  ?>
 <div class="content_3">
     <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
     <div class="caption caption_2">
-        <span>At Dreamscapers Publishing Inc. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
+        <span>At Dreamscaper Publishing Inc. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
             strive to someday be acquired by a monopoly corporation. <br> We believe in the power of the Interferes
             Asset as the foundation for wealthy-building in both the real and metaverse, and we’re exited to play
             an active role in ownership economy.</span>
@@ -96,7 +96,7 @@ get_header();  ?>
                     <img src="<?php echo get_template_directory_uri(); ?>/inc/img/section_4.png" alt="">
                 </td>
                 <td class="sec_4_cap">
-                    <span>At Dreamscapers Publishing Inc. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
+                    <span>At Dreamscaper Publishing Inc. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
                         strive to someday be acquired by a monopoly corporation. <br> We believe in the power of the Interferes
                         Asset as the fi=foundation for wealthy-building in both the real and metaverse, and we’re exited to play
                         an active role in ownership economy.</span>
