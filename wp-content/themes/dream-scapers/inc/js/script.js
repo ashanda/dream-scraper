d4f66973625f1e3c1721567c7ba376bf9d07b62a
Kasun24 <kasun961224@gmail.com>
2022-09-03 20:03:57 +0530

    test

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -50,8 +50,8 @@ var height = Math.max(body.scrollHeight, body.offsetHeight,
 if (window.innerWidth <= 768) {
     window.onscroll = function (event) {
         var x = window.pageYOffset;
-        parallax_1.style.backgroundPositionY = (((x*5) / 100) - 10) + 'vw';
-        parallax_2.style.backgroundPositionY = (((x*2) / 100) - 18) + 'vw';
+        parallax_1.style.backgroundPositionY = (((x-2) / 100) - 10) + 'vw';
+        parallax_2.style.backgroundPositionY = (((x) / 100) - 18) + 'vw';
         parallax_3.style.backgroundPositionY = (((x*2) / 100) - 34) + 'vw';
         parallax_4.style.backgroundPositionY = (((x*2) / 100) + 10) + 'vw';
     }
