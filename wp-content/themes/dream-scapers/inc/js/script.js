5f14d3767b8852fed94a6f9b810ff6bb8aa3ac6a
Kasun24 <kasun961224@gmail.com>
2022-09-03 11:17:38 +0530

    test

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -34,13 +34,7 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-function scroll_function() {
-    const boxes = document.querySelectorAll('.parallax');
 
-    boxes.forEach(box => {
-        box.style.width = '300px';
-    });
-}
 // function closePreloader() {
 
 //     // const element = document.getElementById("preloader");
