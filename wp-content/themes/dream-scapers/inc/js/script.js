3c4ab8e686a1ba20e43f6b63d62bd34146c8aaa0
Kasun24 <kasun961224@gmail.com>
2022-09-03 17:35:55 +0530

    test

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -43,11 +43,12 @@ var parallax_4 = document.getElementById('parallax_4');
 // trigger this function every time the user scrolls
 window.onscroll = function (event) {
     var x = window.pageYOffset;
-        // parallax_1.style.backgroundPositionY = ((x/100)-5)+'vw';
-        parallax_1.style.backgroundPositionY = (x-x)+'vw';
-        parallax_2.style.backgroundPositionY = ((x/100)-10)+'vw';
-        parallax_3.style.backgroundPositionY = ((x/100)-15)+'vw';
-        parallax_4.style.backgroundPositionY = (x/100)+'vw';
+    var y = document.height;
+    // parallax_1.style.backgroundPositionY = ((x/100)-5)+'vw';
+    parallax_1.style.backgroundPositionY = (y - x) + 'vw';
+    parallax_2.style.backgroundPositionY = ((x / 100) - 10) + 'vw';
+    parallax_3.style.backgroundPositionY = ((x / 100) - 15) + 'vw';
+    parallax_4.style.backgroundPositionY = (x / 100) + 'vw';
 }
 
 
