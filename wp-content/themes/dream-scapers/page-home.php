09b45984f9791b3053999be90b80687ee2cd2dcd
Kasun24 <kasun961224@gmail.com>
2022-09-03 17:24:26 +0530

    test

--- wp-content/themes/dream-scapers/page-home.php
+++ wp-content/themes/dream-scapers/page-home.php
@@ -64,12 +64,12 @@ get_header();  ?>
 <div class="content_2">
     <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
     <div class="caption caption_1">
-        <span>In a world driven by value-extracion and exploitative principles, we, here at Dreamscapers Publishing Inc,
-            Inc., are building an ecosystem grounded in contribute and collaborative value principles. <br> By
+        <span>In a world driven by value-extraction and exploitative principles, we, here at Dreamscapers Publishing Inc,
+            Inc., are building an ecosystem grounded in contributive and collaborative value principles. <br> By
             contributing and leveraging Our entire intellectual Property portfolio (from technology-related to
             brand-related and artistic-related IP), and sharing our earnings for the benefit of the many rather that
-            the few, we are devoted to overhauling Shared Capitalism , and BE the change we want to see both og our
-            worlds, physical and disgital. </span>
+            the few, we are devoted to overhauling Shared Capitalism , and BE the change we want to see in both of our
+            worlds, physical and digital. </span>
 
     </div>
 </div>
@@ -87,84 +87,6 @@ get_header();  ?>
 <img class="devider" src="<?php echo get_template_directory_uri(); ?>/inc/img/devider_white.png');" alt="">
 <!-- Content section 4 -->
 <div class="content_4">
-    <!-- <div class="container">
-        <div class="d-sec mt-4">
-            <div class="tab">
-                <button class="tablinks" onclick="openCity(event, 'year1')">Tab 1</button>
-                <button class="tablinks" onclick="openCity(event, 'year2')">Tab 2</button>
-                <button class="tablinks" onclick="openCity(event, 'year3')">Tab 3</button>
-                <button class="tablinks" onclick="openCity(event, 'year4')">Tab 4</button>
-            </div>
-
-            <div id="year1" class="dy-default tabcontent">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="dy-img">
-                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/section_4.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="dy-caption">
-                            <div class="mb-2"><span class="gold">The Interverse Asset <sup>TM</sup></span></div>
-                            <p>The Interverse Asset<sup>TM</sup> is Dreamscapers Publishing’s trademark non-depreciable compact asset that provides economic and social utilities in both, our Real, tangible world, and the digital/metaverse.
-                            </p>
-                            <p>Dreamscapers Publishing launches The HoloNovel<sup>TM</sup> as its inaugural Interverse AssetTM. This inaugural collection is limited to only 2,000 HoloNovelTM volumes worldwide.</p>
-                            <p>The Interverse Asset<sup>TM</sup> is comprised of three interoperable yet distinct components/assets that generate surplus value:</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="year2" class="tabcontent">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="dy-img">
-                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/section_4.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="dy-caption">
-                            <div class="mb-2"><span class="gold">The Physical Asset</span></div>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos unde rem qui similique inventore animi distinctio, nesciunt saepe fugiat? Enim magni animi alias nihil blanditiis beatae aliquid doloribus, numquam in.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="year3" class="tabcontent">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="dy-img">
-                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/section_4.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="dy-caption">
-                            <div class="mb-2"><span class="gold">The Limited Digital Asset (LDA)</span></div>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa accusamus, animi esse nisi nemo, harum est voluptates doloremque saepe aliquam nobis beatae quam rem. Dignissimos quas illo consequatur voluptates aspernatur.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div id="year4" class="tabcontent">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="dy-img">
-                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/key_light.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="dy-caption">
-                            <div class="mb-2"><span class="gold">The Royalty Entitlement</span></div>
-                            <p>At Dreamscapers Publishing Inc. We ‘re not interested in an “exit strategy,” We don’t seek an IPO nor do we
-                                strive to someday be acquired by a monopoly corporation. <br> We believe in the power of the Interferes
-                                Asset as the fi=foundation for wealthy-building in both the real and metaverse, and we’re exited to play
-                                an active role in ownership economy.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
     <div class="container accordion_section">
         <button class="accordion">The Interverse Asset <sup>TM</sup></button>
         <div class="panel" style="display: block;">
@@ -239,24 +161,9 @@ get_header();  ?>
             </div>
         </div>
     </div>
-
-    <!-- <img class="white_sec" src="<?php echo get_template_directory_uri(); ?>/inc/img/white_section.png" alt="">
-    <div class="caption caption_3">
-        <span class="gold">The Royalty Entitlement</span>
-        <table width="100%">
-            <tr>
-                <td class="sec_4_img">
-                    <img src="<?php echo get_template_directory_uri(); ?>/inc/img/section_4.png" alt="">
-                </td>
-                <td class="sec_4_cap">
-                    <span></span>
-                </td>
-            </tr>
-        </table>
-    </div> -->
 </div>
 
-
+<!-- test -->
 
 
 <?php get_footer(); ?>
