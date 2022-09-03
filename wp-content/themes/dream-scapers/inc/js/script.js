d339b2d4a4fa421be05a854a6cdf0c48fe357f9d
Kasun24 <kasun961224@gmail.com>
2022-09-03 12:21:47 +0530

    test

--- wp-content/themes/dream-scapers/inc/js/script.js
+++ wp-content/themes/dream-scapers/inc/js/script.js
@@ -35,30 +35,30 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-var body = document.getElementsByTagName('body')[0];
-        body.style.backgroundColor = 'green';
+var parallax = document.getElementsByClassName('parallax')[0];
+        parallax.style.backgroundColor = 'green';
 
         // trigger this function every time the user scrolls
         window.onscroll = function (event) {
             var scroll = window.pageYOffset;
             if (scroll < 300) {
                 // green
-                body.style.backgroundColor = 'green';
+                parallax.style.backgroundColor = 'green';
             } else if (scroll >= 300 && scroll < 600) {
                 // yellow
-                body.style.backgroundColor = 'yellow';
+                parallax.style.backgroundColor = 'yellow';
             } else if (scroll >= 600 && scroll < 1200) {
                 // blue
-                body.style.backgroundColor = 'blue';
+                parallax.style.backgroundColor = 'blue';
             } else if (scroll >= 1200 && scroll < 1800) {
                 // orange
-                body.style.backgroundColor = 'orange';
+                parallax.style.backgroundColor = 'orange';
             } else if (scroll >= 1800 && scroll < 3000) {
                 // red
-                body.style.backgroundColor = 'red';
+                parallax.style.backgroundColor = 'red';
             } else {
                 // purple
-                body.style.backgroundColor = 'purple';
+                parallax.style.backgroundColor = 'purple';
             }
         }
 
