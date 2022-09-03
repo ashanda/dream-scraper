d1eac27dc17597e06094a9dee005494ba30472e2
Kasun24 <kasun961224@gmail.com>
2022-09-03 11:35:55 +0530

    test

--- wp-content/themes/dream-scapers/footer.php
+++ wp-content/themes/dream-scapers/footer.php
@@ -37,7 +37,7 @@
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <?php /*
     <script src="<?php echo get_template_directory_uri(); ?>/inc/js/script.js"></script>
     */ ?>
