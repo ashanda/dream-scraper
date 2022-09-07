f67b63c34cac48430cb7e880e4986ce294765a2f
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-09-07 15:03:25 +0530

    Exclusive Value text  change

--- wp-content/themes/dream-scapers/404.php
+++ wp-content/themes/dream-scapers/404.php
@@ -20,38 +20,11 @@ get_header();
 			<div class="page-content">
 				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'dream-scapers' ); ?></p>
 
-					<?php
-					get_search_form();
+				<div class="widget widget_categories">
+						
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'dream-scapers' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$dream_scapers_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'dream-scapers' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$dream_scapers_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
+				</div><!-- .page-content -->
+		    </div>
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
