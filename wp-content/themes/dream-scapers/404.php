6767b0fdc656b1836cd26a217686c0412598419e
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2022-09-07 15:05:11 +0530

    Exclusive Value text  change

--- wp-content/themes/dream-scapers/404.php
+++ wp-content/themes/dream-scapers/404.php
@@ -13,12 +13,12 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
+			
+
+			<div class="page-content">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dream-scapers' ); ?></h1>
 			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'dream-scapers' ); ?></p>
 
 				<div class="widget widget_categories">
 						
