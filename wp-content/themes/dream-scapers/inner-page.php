29ae03d78e4f6ccb5e3c2b326bb6ccf906cf97fd
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-09-08 15:00:01 +0530

    test

--- wp-content/themes/dream-scapers/inner-page.php
+++ wp-content/themes/dream-scapers/inner-page.php
@@ -8,8 +8,37 @@ get_header();
 ?>
 <br>
 </div>
-<div class="">
-    sample content
+<div class="container-fluid inner_page_body">
+    <div class="row">
+        <div class="col-lg-7 col-md-7 col-sm-6">
+            <div class="left_content">
+                <div class="header">
+                    <h2>WELCOME TO THE BORED APE YACHT CLUB</h2>
+                </div>
+                <div class="content">
+                    <p>BAYC is a collection of 10,000 Bored Ape NFTs—unique digital collectibles living on the Ethereum blockchain. Your Bored Ape doubles as your Yacht Club membership card, and grants access to members-only benefits, the first of which is access to THE BATHROOM, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-6">
+            <div class="right_content">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/heart.png" alt="">
+                    </div>
+                    <div class="col-sm-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/heart.png" alt="">
+                    </div>
+                    <div class="col-sm-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/heart.png" alt="">
+                    </div>
+                    <div class="col-sm-6">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/heart.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
